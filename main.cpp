fdac6e60bac20dba6d0f5af92d80c16d67d4cc8b
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2024-01-08 18:13:55 -0300

    BeeCrownd 1012

--- main.cpp
+++ main.cpp
@@ -1,64 +1,15 @@
-/******************************************************************************
+#include <stdio.h>
 
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
+    int main()
+    {
+     double a, b, c;
 
-*******************************************************************************/
-#include<stdio.h>
-#include <iostream>
+     scanf("%lf %lf %lf", &a, &b, &c);
+     printf("TRIANGULO: %.3lf\n", (a * c) / 2);
+     printf("CIRCULO: %.3lf\n", c * c * 3.14159);
+     printf("TRAPEZIO: %.3lf\n", ((a + b) / 2) * c);
+     printf("QUADRADO: %.3lf\n", b * b);
+     printf("RETANGULO: %.3lf\n", a * b);
 
-using namespace std;
-
-int main()
-{
-    float salario, novosalario, reajuste; 
-    
-    scanf("%f.2",&salario);
-    
-    if(salario <= 400 ){
-        reajuste = salario * 0.15;
-        novosalario = salario + reajuste;
-        
-        printf("Novo salario: %.2f\n", novosalario);
-        printf("Reajuste ganho: %.2f\n", reajuste);
-        printf("Em percentual: 15 %%\n");
-        
-        
-    } else if (salario > 400.01 && salario <= 800){
-        reajuste = salario * 0.12;
-        novosalario = salario + reajuste;
-        
-        printf("Novo salario: %.2f\n", novosalario);
-        printf("Reajuste ganho: %.2f\n", reajuste);
-        printf("Em percentual: 12 %%\n");
-        
-        
-    } else if (salario > 800.01 && salario <= 1200){
-        reajuste = salario * 0.10;
-        novosalario = salario + reajuste;
-        
-        printf("Novo salario: %.2f\n", novosalario);
-        printf("Reajuste ganho: %.2f\n", reajuste);
-        printf("Em percentual: 10 %%\n");
-        
-    
-    } else if (salario > 1200.01 && salario <= 2000){
-        reajuste = salario * 0.07;
-        novosalario = salario + reajuste;
-        
-        printf("Novo salario: %.2f\n", novosalario);
-        printf("Reajuste ganho: %.2f\n", reajuste);
-        printf("Em percentual: 7 %%\n");
-
-    }else {
-        reajuste = salario * 0.04;
-        novosalario = salario + reajuste;
-        
-        printf("Novo salario: %.2f\n", novosalario);
-        printf("Reajuste ganho: %.2f\n", reajuste);
-        printf("Em percentual: 4 %%\n"); 
-    }
-    
-    return 0;
-}
+     return 0;
+    }         
